2e88ad34143590a184983da1da28fd03cf95f1e8
gabriel-mitzap <110055670+gabriel-mitzap@users.noreply.github.com>
2026-01-02 18:05:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -975,3 +975,27 @@ with tab_data:
             st.session_state[DF_KEY] = cleaned
             st.session_state[EDITOR_VERSION_KEY] += 1
             st.rerun()
+
+st.markdown('### Already have an Account? Login Below👇🏻')
+with st.form("login_form"):
+    st.write("Login")
+    email = st.text_input('Enter Your Email')
+    password = st.text_input('Enter Your Password')
+    submitted = st.form_submit_button("Login")
+
+
+if submitted:
+    if password == config('SECRET_PASSWORD'):
+        st.session_state['logged_in'] = True
+        st.text('Succesfully Logged In!')
+    else:
+        st.text('Incorrect, login credentials.')
+        st.session_state['logged_in'] = False
+
+
+if 'logged_in' in st.session_state.keys():
+    if st.session_state['logged_in']:
+        st.markdown('## Ask Me Anything')
+        question = st.text_input('Ask your question')
+        if question != '':
+            st.write('I drink and I know things.')
